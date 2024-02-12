0465bfa762582fbd2ca2779a1b6fdbd121a1bbdc
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-02-13 00:26:28 +0500

    Update 70. Climbing Stairs.c

--- Easy Problems/70. Climbing Stairs.c
+++ Easy Problems/70. Climbing Stairs.c
@@ -1,1 +1,10 @@
-
+int climbStairs(int n) {
+    int i = 0, ways[n+1];
+    ways[0] = 1;
+    ways[1] = 1;
+    for(i = 2; i <= n; i++)
+    {
+        ways[i] = ways[i-1] + ways[i-2];
+    }
+    return ways[n];
+}
