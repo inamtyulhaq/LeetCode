243b3f287a6f1edc0b4ee20506f80debc88bf48f
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-02-13 11:59:28 +0500

    Create 367. Valid Perfect Square.c

---
+++ Easy Problems/367. Valid Perfect Square.c
@@ -0,0 +1,16 @@
+bool isPerfectSquare(int num) {
+    bool choice = false;
+    for(long int i = 1; i <= num; i++)
+    {
+        long int j = i * i;
+        if(num == j)
+        {
+            choice = true;
+            break;
+        }
+        else if (num < j){
+            break;
+        }
+    }
+    return choice;
+}
