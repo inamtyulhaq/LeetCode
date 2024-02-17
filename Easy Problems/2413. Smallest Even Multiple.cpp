84e5b69f3313c57d4fbed4f2964192146e89d1c4
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-02-17 22:29:35 +0500

    Create 2413. Smallest Even Multiple.cpp

---
+++ Easy Problems/2413. Smallest Even Multiple.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        int x;
+        for(int i = 1; i <= 300; i++)
+        {
+            if(i % n == 0 && i % 2 == 0)
+            {
+                x = i;
+                break;
+            }
+        }
+        return x;
+    }
+};
