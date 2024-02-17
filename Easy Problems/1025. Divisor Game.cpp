91b5a60cbd497caf31c777500aa0f3c4fc1bb3de
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-02-17 22:27:37 +0500

    Create 1025. Divisor Game.cpp

---
+++ Easy Problems/1025. Divisor Game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool divisorGame(int n)
+{
+    if(n <= 1)
+    {
+        return false;
+    }
+    for(int x = 1; x < n; x++)
+    {
+        if(n % x == 0)
+        {
+            return !divisorGame(n-x);
+        }
+    }
+    return false;
+}
+};
