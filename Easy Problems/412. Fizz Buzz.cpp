23a316817fa2f82dd6efdc6e9a0968a480108afe
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-03-10 18:39:59 +0500

    Create 412. Fizz Buzz.cpp

---
+++ Easy Problems/412. Fizz Buzz.cpp
@@ -0,0 +1,40 @@
+// Question
+// https://leetcode.com/submissions/detail/1193707065/
+
+// Explanation:
+// took a vector
+// iterated from 1 till n, given.
+// if any number is divisible by 3 and 5, pushed FizzBuzz to vector
+// else if divisible by 3, pushed back Fizz
+// else if divisible by 5, pushed back Buzz
+// else if no as such condition met, simply pushed that number
+// output the vector
+
+// Solution:
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) 
+    {
+        vector<string>vec;
+        for(int i = 1; i <= n; i++)
+        {
+            if(i%3==0 && i%5==0)
+            {
+                vec.push_back("FizzBuzz");
+            }
+            else if(i%3==0)
+            {
+                vec.push_back("Fizz");
+            }
+            else if(i%5==0)
+            {
+                vec.push_back("Buzz");
+            }
+            else
+            {
+                vec.push_back(to_string(i));
+            }
+        }
+        return vec;
+    }
+};
