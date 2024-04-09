36547db8b154bc4f5a3886e2ac77cc7b9f9bc55e
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-04-09 11:37:11 +0500

    Create 2769. Find the Maximum Achievable Number.cpp

---
+++ Easy Problems/2769. Find the Maximum Achievable Number.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/find-the-maximum-achievable-number/
+
+//
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        int result = 0, n = num;
+        while(t--)
+        {
+            num++;
+        }
+        int n1 = num - n;
+        result = num + n1;
+        return result;
+    }
+};
