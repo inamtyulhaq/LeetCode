4b30f47d39de9e78d91feb32be155bd5605d38b5
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-05-06 21:41:03 +0500

    Create 771. Jewels and Stones.cpp

---
+++ Easy Problems/771. Jewels and Stones.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/jewels-and-stones/description/ 
+
+// The problem is way simpler
+// Simply use a nested loop and
+// compare every character of one string with every character of another string
+// convert every character to its ascii (int) before comparing
+// icrement the count, if match is found
+
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count = 0;
+        for(int i = 0; i < jewels.length(); i++)
+        {
+            int a = jewels[i];
+            for(int j = 0; j < stones.length(); j++)
+            {
+                int b = stones[j];
+                if(a == b)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
