4fc4d81ae2bb855f4bc3829149c15e723f9c5b13
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-05-11 13:12:28 +0500

    Create 50. Pow(x, n).cpp

---
+++ Medium Problems/50. Pow(x, n).cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/powx-n/description/
+
+// simply implement pow(x,n)
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double c = pow(x,n);
+        return c; 
+    }
+};
