a0da6b21040156a353e062d78d6fdc0448ba4fff
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-06-30 12:59:22 +0500

    Create 595. Big Countries.sql

---
+++ Easy Problems/595. Big Countries.sql
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/big-countries/
+
+-- solved in ORACLE SQL
+-- do focus that select only those names, populations and areas where either area is greater than 3000000
+-- OR
+-- population is greater than 25000000
+
+/* Write your PL/SQL query statement below */
+SELECT name, population, area FROM World WHERE area >=3000000 OR population >=25000000
