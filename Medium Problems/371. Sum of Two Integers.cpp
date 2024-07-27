47b0c53234cc74e24a8226a2baf1c97e66942c13
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-07-27 11:40:54 +0500

    Create 371. Sum of Two Integers.cpp

---
+++ Medium Problems/371. Sum of Two Integers.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/sum-of-two-integers/description/
+
+// simply return the sum
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        return a+b;
+    }
+};
