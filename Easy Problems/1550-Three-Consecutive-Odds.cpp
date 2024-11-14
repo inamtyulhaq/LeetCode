58b170e8b62043868f4df8d14664db0c227ec9fa
inamtyulhaq <inamtyulhaq@gmail.com>
2024-11-14 20:59:33 +0500

    Three-Consecutive-Odds Problem (O(n))

---
+++ Easy Problems/1550-Three-Consecutive-Odds.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/three-consecutive-odds/
+
+// Iterate through the array from index 0 till n-2
+// see if any three consecutive values divisble by 0 don't give 0 as remainder
+// return true if any (they are old)
+// else return false
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        for(int i = 0; i < arr.size() - 2; i++)
+        {
+            if(arr[i] % 2 != 0 && arr[i+1] % 2 != 0 && arr[i+2] % 2 != 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
